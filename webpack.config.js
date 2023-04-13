6d84d20b7307406c513d0d35f22696e99bdbfc07
vrbdas <vrbdas@gmail.com>
2023-04-13 18:44:38 +0600

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let path = require('path');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
@@ -14,7 +14,25 @@ module.exports = {
   devtool: 'source-map',
 
   module: {
-
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: 'ie 11',
+                debug: true,
+                corejs: 3.30,
+                useBuiltIns: 'usage',
+              }],
+            ],
+          },
+        },
+      },
+    ],
   },
 
 };
